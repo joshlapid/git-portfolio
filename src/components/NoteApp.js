e9f1f5895eb6187ba3a164736fa137f8059e978f
joshlapid <lapidjoshua@gmail.com>
2019-06-24 16:07:33 -0700

    first commit

---
+++ src/components/NoteApp.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect, useReducer } from 'react';
+import notesReducer from '../reducers/notes';
+import NoteList from './NoteList';
+import AddNoteForm from './AddNoteForm';
+import NotesContext from '../context/notes-context';
+
+const NoteApp = () => {
+    const [notes, dispatch] = useReducer(notesReducer, []);
+
+    useEffect(() => {
+        const notesData = JSON.parse(localStorage.getItem('notes'));
+
+        if (notesData) {
+            dispatch({ type: 'POPULATE_NOTES', notes: notesData })
+            // setNotes(notesData);
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('notes', JSON.stringify(notes));
+    }, [notes]);
+
+    return (
+        <NotesContext.Provider value={{ notes, dispatch }}>
+            <h1>Notes</h1>
+            <NoteList />
+            <AddNoteForm />
+        </NotesContext.Provider>
+    )
+}
+
+export { NoteApp as default };
